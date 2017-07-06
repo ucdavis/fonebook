428949305814a170ba00634b301850f54c161dbe
Aaron <aaron@campus-028-154.ucdavis.edu>
2017-07-06 11:48:02 -0700

    Added workgroups and database library

--- server/app.js
+++ server/app.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const mysql = require('mysql')
+const bodyParser = require('body-parser');
+
 const app = express();
 
+app.use(bodyParser.json()); // for parsing application/json
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'fonebook'
+});
+	
 app.get('/', function (req, res) {
 	res.send('Hello World!');
 });
@@ -8,9 +20,39 @@ app.get('/', function (req, res) {
 app.get('/status', function (req, res) {
 	res.status(200).send('A-Okay');
 });
+app.get('/workgroups', function (req,res) {
+
+	connection.connect()
+	var workgroups = [];
+	connection.query('SELECT * from workgroups', function (err, rows, fields) {
+	  if (err) throw err
+	
+	  workgroups = rows;
+	  res.status(200).send(workgroups);
+	})
+	
+	connection.end()
+
+});
+
+
+app.post('/workgroups', function (req,res) {
+
+	connection.connect()
+	
+	connection.query('insert into workgroups (name,created_at,updated_at) values (?,now(),now())',[req.body.name], function (err, rows, fields) {
+	  if (err) throw err
+	
+	  res.status(200).send();
+	})
+	
+	connection.end()
+
+});
+
 
 app.listen(3000, function () {
 	console.log('Example app listening on port 3000!');
 });
 
-console.log("Program Done");
+
