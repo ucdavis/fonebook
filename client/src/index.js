1f254ff78f129f40f179c6e33881280401cc1d89
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2017-08-25 09:44:48 -0700

    Make state available to smart components

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
 import './index.css';
 import App from './App';
+import { rootReducer } from './reducers/rootReducer';
+import { getWorkgroupsData } from './actions/workgroupActions';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let store = createStore(
+	rootReducer,
+	applyMiddleware(thunk)
+);
+
+store.dispatch(getWorkgroupsData());
+
+console.log(store.getState());
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root'));
