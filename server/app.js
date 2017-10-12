6957089aba0333111d8c8ee4e09f48a4f093fc78
Christopher Thielen <cmthielen@ucdavis.edu>
2017-10-12 11:05:45 -0700

    Linting fixes

--- server/app.js
+++ server/app.js
@@ -7,9 +7,9 @@ const app = express();
 app.use(bodyParser.json());
 
 // Enable CORS
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
