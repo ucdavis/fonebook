fc55c1a37ec1cf853655b4017f784b1290870437
Christopher Thielen <cmthielen@ucdavis.edu>
2017-10-12 11:05:50 -0700

    Finished workgroup CRUD model

--- server/models/workgroup.js
+++ server/models/workgroup.js
@@ -1,16 +1,19 @@
-"use strict";
-
-module.exports = function(sequelize, DataTypes) {
-  var Workgroup = sequelize.define("Workgroup", {
-    name: DataTypes.STRING
+module.exports = (sequelize, DataTypes) => {
+  const Workgroup = sequelize.define('Workgroup', {
+    name: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: true
+      }
+    }
   }, {
-  	updatedAt:"updated_at",
-  	createdAt:"created_at"
-  	});
+    updatedAt: 'updated_at',
+    createdAt: 'created_at'
+  });
 
 //  Workgroup.associate = function(models) {
 //    Workgroup.hasMany(models.Task);
 //  }
-  
+
   return Workgroup;
 };
--- server/routes/workgroups.js
+++ server/routes/workgroups.js
@@ -1,28 +1,66 @@
 const express = require('express');
-var models = require('../models');
+const models = require('../models');
+const Sequelize = require('sequelize');
 
 const router = express.Router();
 
+// Index
 router.get('/', (req, res) => {
-	models.Workgroup.findAll().then(function(workgroups){
-		res.status(200).send(workgroups);
-	});
-});
-// create
-router.post('/', (req, res, next) => {
-	models.Workgroup.create({
-		name:req.body.name
-	}).then(() => res.status(200).send());
-});
-// delete
-router.delete('/:id', (req, res, next) => {
-	models.Workgroup.destroy({
-		where:{
-			id:req.params.id
-		}
-	}).then(() => res.status(200).send());
+  models.Workgroup.findAll().then((workgroups) => {
+    res.status(200).send(workgroups);
+  });
 });
 
+// Create
+router.post('/', (req, res) => {
+  models.Workgroup.create({
+    name: req.body.name
+  }).then(() => res.status(200).send());
+});
 
+// Delete
+router.delete('/:id', (req, res) => {
+  models.Workgroup.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(() => res.status(200).send());
+});
+
+// Show
+router.get('/:id', (req, res) => {
+  models.Workgroup.findById(req.params.id).then((workgroup) => {
+    if (workgroup == null) {
+      res.status(404).send();
+    } else {
+      res.status(200).send(workgroup);
+    }
+  });
+});
+
+// Update
+router.put('/:id', (req, res) => {
+  models.Workgroup.findById(req.params.id).then((workgroup) => {
+    if (workgroup == null) {
+      res.status(404).send();
+    } else {
+      if (req.body.name === undefined) {
+        res.status(400).send();
+        return;
+      }
+
+      const updatedWorkgroup = workgroup;
+      updatedWorkgroup.name = req.body.name;
+
+      updatedWorkgroup.save().then(
+        () => res.status(200).send(updatedWorkgroup)
+      ).catch(Sequelize.ValidationError, () => {
+        res.status(400).send();
+      }).error(() => {
+        res.status(500).send();
+      });
+    }
+  });
+});
 
 module.exports = router;
