9de7a086f1bf17d323e0ee9d5360e116d1cd5c98
Mauricio <mromeroval@ucdavis.edu>
2017-10-26 10:31:05 -0700

    Deleted old migrations and sql files for dbmigrate

--- server/migrations/sqls/20170831180101-create-workgroups-up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `workgroups` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(96) NOT NULL,
-  `created_at` datetime DEFAULT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
--- server/migrations/sqls/20170831183347-create-office-hours-down.sql
+++
@@ -1,1 +0,0 @@
-DROP TABLE `office_hours`;
